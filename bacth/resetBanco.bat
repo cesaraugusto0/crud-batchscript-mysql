2c3a1d06c9b933157a202b1e741f1b8b257ad501
Cesar Augusto <augustocsar22@gmail.com>
2022-08-20 09:06:40 -0300

    resetBanco.bat pronto

--- bacth/resetBanco.bat
+++ bacth/resetBanco.bat
@@ -1,5 +1,11 @@
+@echo off
+
 cd \xampp\mysql\bin
 
 mysql -u root < \cpultima\sql\resetBanco.sql
+cls
+
+echo Reset realizado com sucesso!
+echo.
 
 pause
--- sql/resetBanco.sql
+++ sql/resetBanco.sql
@@ -1,22 +1,41 @@
 
-
-select exists (
-    SELECT schema_name
-    FROM information_schema.schemata
-    where schema_name="cpultima"
-    ) as ja_existe;
-
-
-drop database cpultima;
+-- drop database cpultima;
 
 create database cpultima;
 
 use cpultima;
 
-create table PC(id_pc int auto_increment, processador varchar(20), memoria varchar(10), hd varchar(10), primary key (id_pc));
-create table OS(id_os int auto_increment, id_pc int, id_m int, primary key(id_os));
-create table Manutencao(id_m int auto_increment, id_os int, quantidade_pc int, sintoma varchar(50),  data_rec date, primary key (id_m));
-create table Termino (id_t int auto_increment, id_m int, solucao varchar(50), termino date, primary key(id_t));
+create table PC(
+    id_pc int auto_increment, 
+    processador varchar(20), 
+    memoria varchar(10), 
+    hd varchar(10), 
+    primary key (id_pc)
+);
+
+create table OS(
+    id_os int auto_increment, 
+    id_pc int, 
+    id_m int, 
+    primary key(id_os)
+);
+
+create table Manutencao(
+    id_m int auto_increment, 
+    id_os int, 
+    quantidade_pc int, 
+    sintoma varchar(50),  
+    data_rec date, 
+    primary key (id_m)
+);
+
+create table Termino (
+    id_t int auto_increment, 
+    id_m int, 
+    solucao varchar(50), 
+    termino date, 
+    primary key(id_t)
+);
 
 
 
